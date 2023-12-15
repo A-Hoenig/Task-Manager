ac14867a2188815f77671c7ac93fe670fde9b828
Alex Hoenig <delimey@pm.me>
2023-12-15 09:17:22 +0000

    initial hello world working setup

---
+++ taskmanager/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# only import on local install if env.py is avaiable
+if os.path.exists("env.py"):
+    import env  # noqa
+
+# create instance of imported Flask class
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+# create instance of SQLAlchemy class
+db = SQLAlchemy(app)
+
+# import last as app and db variables needed
+from taskmanager import routes  # noqa
