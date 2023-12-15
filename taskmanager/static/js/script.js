d1310bf1c8dea64ce2284d1bece4282eb38b26f3
Alex Hoenig <delimey@pm.me>
2023-12-15 16:47:47 +0000

    add task fucntionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     //sidenav bar initialization
-    var sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // dropdown initialization
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
